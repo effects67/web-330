256172a0b9cd0541804702b56625216e8dd2ae36
effects67 <32003168+effects67@users.noreply.github.com>
2018-03-13 20:00:34 -0500

    adding 3.2
    3.3 needs to be fixed

--- week-3/Vance-3.2.js
+++ week-3/Vance-3.2.js
@@ -1,6 +1,4 @@
 var header = require('../header.js');
-console.log(header.display("Exercise 3.2", "Gabriel", "Vance", "The Factory Pattern")); 
-
 
 /*
  Expected output:
@@ -24,12 +22,6 @@ console.log(header.display("Exercise 3.2", "Gabriel", "Vance", "The Factory Patt
 
 */
 
-/* factory patterns creation pattern creating objects. conditionals with if statements
-generic way for work flows in application
-
-goal: add 2 additional function patterns, add 2 classes and utilize output*/
-
-
 // start program
 function Postgres(properties) {
     this.username = properties.username || "admin";
@@ -44,43 +36,14 @@ function MySql(properties) {
     this.version = properties.version || 5.7
 }
 
-// create 2 function classes oracle and informix
-// add values with ||
-
-function Oracle (properties){
-    this.username = properties.username || 'hero';
-    this.password = properties.password || 'hero-passowrd';
-    this.server = properties.server|| 'localhost:5454';
-    this.version = properties.version || '10g';
-}
-
-function Informix (properties){
-    this.username = properties.username || 'sidekick';
-    this.password = properties.password || 'sidekick-password';
-    this.server = properties.server || 'localhost:3030';
-
-}
-
-
-// update method to include these 2 objects (oracle/informix)
 function DatabaseFactory() {}
-//update if statement with MySql then adding other objects
+
 DatabaseFactory.prototype.databaseClass = MySql;
 DatabaseFactory.prototype.createDatabase = function(properties) {
     if (properties.databaseType === "Postgres") {
         this.databaseClass = Postgres;
-    } 
-    if (properties.databaseType ==='MySql') {
+    } else {
         this.databaseClass = MySql;
-    }
-
-    //adding 2 new objects
-    if (properties.databaseType ==='Oracle') {
-        this.databaseClass = Oracle;
-    }
-
-    if (properties.databaseType ==='Informix') {
-        this.databaseClass = Informix;
     }
 
     return new this.databaseClass(properties);
@@ -100,28 +63,11 @@ var mySql = mySqlFactory.createDatabase({
     password: "password"
 });
 
-// add 2 additional databasefactory objects
-//call create databse functions w/ falues
-var oracleFactory = new DatabaseFactory();
-var oracle = oracleFactory.createDatabase({
-    databaseType: 'Oracle',
-    username: "username",
-    password: "password"
-});
-
-var informixFactory = new DatabaseFactory();
-var informix = informixFactory.createDatabase({
-    databaseType: 'Informix',
-    username: "username",
-    password: "password"
-});
 
 
 
-//console.log functions with objects
 
-console.log(oracle);
-console.log(informix);
+
 // end program
 
 
--- week-3/Vance-3.3.js
+++ week-3/Vance-3.3.js
@@ -1,6 +1,4 @@
 var header = require('../header.js');
-console.log(header.display("Exercise 3.3", "Gabriel", "Vance", "Singleton Pattern")); 
-
 
 /*
  Expected output:
@@ -12,56 +10,9 @@ console.log(header.display("Exercise 3.3", "Gabriel", "Vance", "Singleton Patter
  Same database instance? true
 
 */
-/* singleton pattern in js program, can be implemented with class method
-that creates new instance of class if one does not already exist
-
-(creates single object at any give time)
-
-manages visibility of an object*/
-//adding 3.3 code
-
-
 
 // start program
 
-
-
-var DatabaseSingleton = (function() {
-    var instance;
-    function createInstance() {
-        var postgresDatabase = new Object("Database instance initialized!");
-        return postgresDatabase;
-}
-// checks to see if instance is active and if  it is, returns object
-    return {
-        getInstance: function() {
-            if (!instance) {
-                instance = createInstance();
-            }
-            return instance;
-        }
-}
-})();
-
-//creating test function
-
-function databaseSingletonTest(){
-    var oracle=DatabaseSingleton.getInstance();
-    var postgres=DatabaseSingleton.getInstance();
-
-    console.log("Same database instance? %s", oracle ===postgres);
-}
-
-databaseSingletonTest();
-/*
-function databaseInit() {
-    var databaseInstance1 = DatabaseSingleton.getInstance();
-    var databaseInstance2 = DatabaseSingleton.getInstance();
-    console.log("One database instance? " + (databaseInstance1 === databaseInstance2));
-}
-
-databaseInit();
-*/
 
 
 // end program
